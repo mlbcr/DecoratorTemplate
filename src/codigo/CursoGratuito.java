77e73beb77bbe43dd07ad2bc209ae6a0c97dc282
Maria Luísa <malubacari@gmail.com>
2026-03-25 11:52:40 -0300

    commit final

--- src/codigo/CursoGratuito.java
+++ src/codigo/CursoGratuito.java
@@ -1,21 +1,26 @@
 package codigo;
 
 public class CursoGratuito extends Curso {
-	
+	// Como o curso é gratuito, o valor é 0
 	protected double valor = 0;
 
     public CursoGratuito() {
+    	// Chama o construtor da classe pai (nome, precoBase, cargaHoraria, duracao)
         super("Introdução básica a C", 0, 20, 7);
+        
+        // Esses são módulos do curso gratuito
         this.modulos.add("O que é C");
         this.modulos.add("Para que serve C");
         this.modulos.add("Como usamos C");
     }
-
+    
+    // Especificação das atividades para as do curso gratuito
     @Override
     protected void aplicarAtividades() {
         System.out.println("Liberando quiz de múltipla escolha simples");
     }
-
+    
+    // O curso gratuito não é avaliado e não emite certificado
     @Override
     protected void aplicarAvaliacoes() {
         System.out.println("Este curso não possui avaliações");
